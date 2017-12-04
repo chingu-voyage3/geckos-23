062fc774379c7fab69673ed65a1ea24e1f6a75a3
Carlos Nah <cnah27@gmail.com>
2017-12-04 10:56:34 +0000

    modifying files

---
+++ quizilla/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import project from './component/project';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <project />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ quizilla/src/component/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class header extends Component {
+  render() {
+    return (
+      <div className="header">
+        
+      </div>
+    );
+  }
+}
+
+export default header;
