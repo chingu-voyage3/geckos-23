0c9a671b44b45d552a257e804c3abe01431f30fb
Carlos Nah <cnah27@gmail.com>
2017-12-05 11:23:04 +0000

    adding new files and features

--- quizilla/src/App.js
+++ quizilla/src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import project from './component/project';
+import Navbar from './Navbar';
+import body from './body';
 import './App.css';
 
+var NavLinks = [
+  {
+    title:'Home',
+    href:'#'
+  },
+  {
+    title:'About',
+    href:'#'
+  },
+  {
+    title : 'Login/SignUp',
+    href:'#'
+  },
+  {
+    title: 'Contact Us',
+    href:'#'
+  }
+  ];
+  
 class App extends Component {
-  render() {
+    
+  render(){
     return (
       <div className="App">
-          <project />
+      <Navbar navData={NavLinks} />
+      <body />
       </div>
     );
   }
