849797b7c08287b3749f90a3d3a39c41faf0cc7c
Mostafa Hesham <m.hesham3001@gmail.com>
2017-12-15 16:15:13 +0200

    try React router v-4

---
+++ quizilla/src/component/home/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Home = (props)=>{
+    return(
+        <h2>Home component</h2>
+    );
+}
--- quizilla/src/index.js
+++ quizilla/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Home} from './component/home/Home';
+import {LandingPage} from './component/landingPage/LandingPage';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter  as Router, Route  } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Routes =(
+        <Router>
+            <div>
+                <Route exact path="/" component={App}/>
+                <Route component={LandingPage}/>
+                <Route path="/home" component={Home}/>
+           </div>    
+        </Router>
+    );
+
+
+ReactDOM.render(Routes, document.getElementById('root'));
 registerServiceWorker();
