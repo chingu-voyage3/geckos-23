2ca4b247fb41bf01550d0029570c255504666269
ra9 <cnah27@gmail.com>
2017-12-29 09:09:55 +0000

    Modifying files

---
+++ client/components/Homepage/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import quizzilla from './img/quizzilla.png';
+
+class Home extends Component {
+    render(){
+        return (
+            
+            <img src={quizzilla} className="img-responsive" alt="" />
+            
+            );
+    }
+}
+
+export default Home;
