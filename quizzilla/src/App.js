2f508bb83ce070466295ae16cea762f5d783061d
ra9 <cnah27@gmail.com>
2018-01-04 01:34:29 +0000

    Adding links to app.js

--- quizzilla/src/App.js
+++ quizzilla/src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Homepage/Navbar';
+import Home from './components/Homepage/Home';
+import quizilla from './components/img/quizzilla.png';
 import './App.css';
+
+
+var NavLinks = [
+  {
+    title:'Home',
+    href:'#'
+  },
+  {
+    title:'About',
+    href:'#'
+  },
+  {
+    title : 'Login/SignUp',
+    href:'#'
+  },
+  {
+    title: 'Contact Us',
+    href:'#'
+  }
+  ];
+  
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+	   <header className="App-header">
+	    <Navbar navData={NavLinks} />
+		 <h1 className="App-title">Welcome to React</h1>
+		   <img src={quizzilla} className="App-logo" alt="logo" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+		  </header>
+	  <div className="jumbotron">
+      <Home />
+	  </div>
       </div>
     );
   }
