69f622c9786e5d46fa94a4cdfc2d92a07ce1469a
Carlos Nah <cnah27@gmail.com>
2018-01-08 14:06:16 +0000

    Fixing Navigation bar

--- quizzilla/src/App.js
+++ quizzilla/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Homepage/Home';
-<<<<<<< HEAD
-
-=======
->>>>>>> c0f1aebaae42434b6ca94385fbf8a533c8b0fe02
+import Header from './components/Homepage/Header';
 import './App.css';
 
 
@@ -11,16 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-	   <Home />
-	   <header className="App-header">
-<<<<<<< HEAD
-	    <Navbar navData={NavLinks} />
-		 <h1 className="App-title">Welcome to Quizzilla</h1>
-=======
+        <Header />
+	       <Home />
+	   <header>
 	     <h1 className="App-title">Welcome to Quizzilla</h1>
->>>>>>> c0f1aebaae42434b6ca94385fbf8a533c8b0fe02
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
 		  </header>
       </div>
